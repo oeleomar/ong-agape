1a34e7beac3a51058cefc7317e0c0cafd5bb9b7d
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-10-13 20:37:40 -0400

    ok

---
+++ assets/src/scroll-animation.js
@@ -0,0 +1,39 @@
+const observerNormal = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-right");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden-right");
+hiddenElements.forEach((el) => observerNormal.observe(el));
+
+const observerLeft = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-left");
+    } else {
+      entry.target.classList.remove("show-left");
+    }
+  });
+});
+
+const hiddenLeft = document.querySelectorAll(".hidden-left");
+hiddenLeft.forEach((el) => observerLeft.observe(el));
+
+const doe = document.querySelector(".doe-anim");
+const observerDoe = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("doe-anim-animate");
+    } else {
+      entry.target.classList.remove("doe-anim-animate");
+    }
+  });
+});
+observerDoe.observe(doe);
